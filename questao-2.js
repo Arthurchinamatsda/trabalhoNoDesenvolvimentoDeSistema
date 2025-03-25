39b2a7bfa36146cd3e8c7547a9ea44eb7a321564
Arthurchinamatsda <ademarteiyuchina@gmail.com>
2025-03-25 12:23:08 -0300

    teste/primeira tentativa com a 1° questao correta

---
+++ questao-2.js
@@ -0,0 +1,12 @@
+//segunda questão
+let filaDeEsperaComLimite = [];
+let limite = 5;
+
+function verificar(i = filaDeEsperaComLimite.length) {
+    if (i < limite) {
+        
+    }
+    else{
+        
+    }
+}
---
+++ questão-1.js
@@ -0,0 +1,7 @@
+//primeira quetão o trabalho
+let carrinhoDeCompras = [];
+carrinhoDeCompras.push("pão"); // ad item
+carrinhoDeCompras.push("leite") //teste para ver se apaga a ultimo
+carrinhoDeCompras.pop(); //remove item
+console.log(carrinhoDeCompras);
+
