ff1f2a8b3f27bc267c9be3816eaddb5237d75618
Arthurchinamatsda <ademarteiyuchina@gmail.com>
2025-05-19 10:49:31 -0300

    Delete questão-1.js

--- questão-1.js
+++
@@ -1,7 +0,0 @@
-//primeira quetão o trabalho
-let carrinhoDeCompras = [];
-carrinhoDeCompras.push("pão"); // ad item
-carrinhoDeCompras.push("leite") //teste para ver se apaga a ultimo
-carrinhoDeCompras.pop(); //remove item
-console.log(carrinhoDeCompras);
-
