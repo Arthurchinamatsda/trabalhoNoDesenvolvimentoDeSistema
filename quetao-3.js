13868c496f97a36613a0995183f577e5d03d61b9
Arthurchinamatsda <ademarteiyuchina@gmail.com>
2025-05-19 10:49:42 -0300

    Delete quetao-3.js

--- quetao-3.js
+++
@@ -1,8 +0,0 @@
-//terceira questao
-const estoqueDPecas = [ "engrenagem" , "mola" , "cano" , "superbonder"]
-function retiraPecas (){
-    estoqueDPecas.unshift("macaco de carro")
-    estoqueDPecas.pop()
-}
-retiraPecas()
-console.log(estoqueDPecas)
